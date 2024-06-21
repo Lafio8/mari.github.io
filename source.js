bf102a4ac804440f524922f14ca8ce6235c81b29
Lafio8 <mfioretti998@gmail.com>
2024-06-21 16:39:59 +0200

    fin

--- source.js
+++ source.js
@@ -1,9 +1,42 @@
+
+let attempts = 0;
+const solution = 'labottigliadivino';
+const hints = [
+    '_ a _ _ _ _ _ _ _ _ _ _ _ _ _ _ _', // 0 lettere
+    '_ a _ _ _ _ i _ _ _ _ _ _ _ _ _ _', // 1 lettera
+    '_ a _ _ _ _ i _ _ _ a _ _ _ _ _ _', // 2 lettere
+    '_ a b _ _ _ i _ _ _ a _ _ _ _ _ _', // 3 lettere
+    '_ a b _ _ _ i _ _ _ a _ i _ _ _ _', // 4 lettere
+    '_ a b _ _ _ i _ _ _ a _ i _ _ n _', // 5 lettere
+    '_ a b _ t _ i _ _ _ a _ i _ _ n _', // 6 lettere
+    '_ a b _ t t i _ _ _ a _ i _ _ n _', // 7 lettere
+    '_ a b _ t t i _ _ _ a _ i _ _ n o', // 8 lettere
+    '_ a b _ t t i _ _ _ a _ i _ i n o', // 9 lettere
+	'_ a b _ t t i _ _ i a _ i _ i n o', // 10 lettere
+	'_ a b _ t t i _ _ i a _ i v i n o', // 11 lettere
+	'_ a b _ t t i g _ i a _ i v i n o', // 12 lettere
+	'_ a b _ t t i g l i a _ i v i n o', // 13 lettere
+	'l a b _ t t i g l i a _ i v i n o', // 14 lettere
+	'l a b o t t i g l i a _ i v i n o', // 15 lettere
+    'l a b o t t i g l i a d i v i n o'  // 16 lettere (soluzione completa)
+];
+
 function verificaRisposta() {
-    const risposta = document.getElementById('risposta').value.toLowerCase();
+    const risposta = document.getElementById('risposta').value.toLowerCase().trim();
     const result = document.getElementById('result');
-    if (risposta === 'la bottiglia di vino') {
-        result.innerHTML = `<p>Brava! Ecco il tuo regalo: <a href="Regalo_Mari.pdf" target="_blank">Clicca qui per aprire il PDF</a></p>`;
+    
+    if (risposta === solution) {
+        result.innerHTML = `<p>Brava MRN! Ti sei guadagnata un <a href="Regalo_Mari.pdf" target="_blank">regalo</a></p>`;
     } else {
-        result.innerHTML = `<p>Risposta sbagliata. Riprova!</p>`;
+        if (attempts < hints.length - 2) {
+            attempts++;
+			result.innerHTML = `<p>"${hints[attempts]}"</p>`;
+			result.innerHTML += `<p>Risposta sbagliata. Riprova!</p>`;
+        }
+		else{
+			attempts++;
+			result.innerHTML = `<p>Sei imbarazzante, era "${hints[attempts]}". Tieni il tuo <a href="Regalo_Mari.pdf" target="_blank">regalo</a></p>`;
+		}
+        
     }
 }
