cf34f7e67e461145f119e7f85ea85a02f4676fa1
Lafio8 <mfioretti998@gmail.com>
2024-06-21 14:47:22 +0200

    commitnew

--- source.js
+++ source.js
@@ -2,7 +2,7 @@ function verificaRisposta() {
     const risposta = document.getElementById('risposta').value.toLowerCase();
     const result = document.getElementById('result');
     if (risposta === 'la bottiglia di vino') {
-        result.innerHTML = `<p>Bravo! Ecco il tuo regalo: <a href="Regalo_Mari.pdf" target="_blank">Clicca qui per aprire il PDF</a></p>`;
+        result.innerHTML = `<p>Brava! Ecco il tuo regalo: <a href="Regalo_Mari.pdf" target="_blank">Clicca qui per aprire il PDF</a></p>`;
     } else {
         result.innerHTML = `<p>Risposta sbagliata. Riprova!</p>`;
     }
